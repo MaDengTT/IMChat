8f4f1f7d41da1690ed7169e24e3cb3e5cfac16d0
MaDengTT <1124879085@qq.com>
2018-10-17 17:01:11 +0800

    建立Circle表

---
+++ common/src/main/java/com/mdshi/common/db/dao/CircleDao.java
@@ -0,0 +1,7 @@
+package com.mdshi.common.db.dao;
+
+/**
+ * Created by MaDeng on 2018/10/17.
+ */
+public interface CircleDao {
+}
---
+++ common/src/main/java/com/mdshi/common/db/entity/CircleEntity.java
@@ -0,0 +1,22 @@
+package com.mdshi.common.db.entity;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+
+import java.util.List;
+
+/**
+ * Created by MaDeng on 2018/10/17.
+ */
+@Entity(tableName = "tb_circle",
+        primaryKeys = {"id"}
+)
+public class CircleEntity {
+
+    long id;
+    long userid;
+    String username;
+    String avatar;
+    String contnent;
+    List<String> images;
+}
